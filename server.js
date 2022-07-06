bebf7e6ee3a742edfd0ad6c5f8744c76365e544c
Waribu George <gwaribu@gmail.com>
2022-07-06 17:55:10 +0300

    middleware css application fixed

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 //load static assets
-app.use("/static", express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, "public")));
 //home route
 app.get("/", (req, res) => {
   res.render("base", { title: "Login System" });
