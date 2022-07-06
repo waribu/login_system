4c48a41c6cf5ebd35312b624309d8b0005fcc523
Waribu George <gwaribu@gmail.com>
2022-07-06 21:04:47 +0300

    logout feature addition

--- router.js
+++ router.js
@@ -13,11 +13,35 @@ router.post("/login", (req, res) => {
     req.body.password == credetial.password
   ) {
     req.session.user = req.body.email;
-    // res.redirect("/dashboard");
-    res.end("Login Successful..!");
+    res.redirect("/route/dashboard");
+    // res.end("Login Successful..!");
   } else {
     res.end("Invalid credetials - Try Again");
   }
+});
+
+//route for Dashboard
+router.get("/dashboard", (req, res) => {
+  if (req.session.user) {
+    res.render("dashboard", { user: req.session.user });
+  } else {
+    res.send("Unauthorized User");
+  }
+});
+
+//route for logout feature
+router.get("/logout", (req, res) => {
+  req.session.destroy(function (err) {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.render("base", {
+        title: "Express",
+        logout: "Logout Successfully...!",
+      });
+    }
+  });
 });
 
 module.exports = router;
